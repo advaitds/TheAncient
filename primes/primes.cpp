4ee999df405cacbfc25c26edc4e90bd033fc46e2
advaitds <advait.salgarkar@gmail.com>
2020-02-16 19:15:00 +0100

    reorganized and changed spacing

--- primes/primes.cpp
+++ primes/primes.cpp
@@ -10,22 +10,21 @@ int main()
     std::cin >> num;
 
     std::cout << "Primes lower than " << num << " are: " << '\n';
-    
+
     // print all the primes numbers below this number
-    
+
     for(int p=2; p<=num; p++)
     {
-		isprime = 1;
-		for(int d=2; d <=sqrt(double(p)); d++)
-		{
-			if(p % d == 0)
-				isprime = 0;
-		}
-		if(isprime == 1)
-			std::cout << p << '\t';
-	}
+        isprime = 1;
+        for(int d=2; d <=sqrt(double(p)); d++)
+        {
+            if(p % d == 0)
+                isprime = 0;
+        }
+        if(isprime == 1)
+            std::cout << p << '\t';
+    }
 
-	std::cout << '\n';
+    std::cout << '\n';
     return 0;
-
 }
