8c377f97f1cd71444cac0a5d07e23bd3210cabcf
advaitds <advait.salgarkar@gmail.com>
2020-03-06 17:58:24 +0100

    array programs

---
+++ arrays/array.c
@@ -0,0 +1,11 @@
+#include <stdio.h>
+#include <curses.h>
+
+void main()
+{
+    int a[10], ii, jj, n;
+    printf("Enter the length of the array: ");
+    scanf("%d", &n);
+    
+    printf("\nYou entered: %d\n", n);
+}
